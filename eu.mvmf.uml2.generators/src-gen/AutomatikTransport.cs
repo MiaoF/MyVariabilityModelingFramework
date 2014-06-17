52ef9d7f798ea670d78e735ce95b0dd6f85137a4
MiaoF <miaofang@gmail.com>
2014-06-17 18:22:36 +0200

    My First Code Generator commit.

---
+++ eu.mvmf.uml2.generators/src-gen/AutomatikTransport.cs
@@ -0,0 +1,8 @@
+     
+     IList<string> locationsAllowedSingleTu =  new List<string>() {
+		"1254","1264","1274",
+	};
+	
+	 IList<string> goodsOutRegistrationLocations =  new List<string>() {
+		"1266","1276","1256",
+	};
