e8f40a35e1817f79eed1144963658cf36c0ef6e6
MiaoF <miaofang@gmail.com>
2016-03-31 22:06:59 +0200

    Add requrestMaterial template to it

--- My Magicdraw Plugin/src/eu/miaofang/md/plugin/model/component/instances/AutomaticSmallPartsStorageComponent.java
+++ My Magicdraw Plugin/src/eu/miaofang/md/plugin/model/component/instances/AutomaticSmallPartsStorageComponent.java
@@ -14,15 +14,19 @@ public class AutomaticSmallPartsStorageComponent extends CompositeComponent{
 		addNewModelType(new WmsModel("AKL-TopologyModel", PlugInConstants.TOPOLOGY_MODEL));
 		addNewModelType(new WmsModel("AKL-OrderProcessing", PlugInConstants.PROCESS_MODEL, "Akl-OrderProcessing-Template"));
 		addNewModelType(new WmsModel("AKL-ActivatePartialOrder", PlugInConstants.PROCESS_MODEL, "Akl-ActivatePartialOrder-Template"));
-		addNewModelType(new WmsModel("AKL-OrderProcessing", PlugInConstants.PROCESS_MODEL, "Akl-OrderProcessing-Template"));
+		addNewModelType(new WmsModel("Akl-RelocateMaterials", PlugInConstants.PROCESS_MODEL, "Akl-RelocateMaterials-Template"));
 		addNewModelType(new WmsModel("AKL-DeliverTablar", PlugInConstants.PROCESS_MODEL, "Akl-DeliverTablar-Template"));
-		addNewModelType(new WmsModel("AKL-GoodsIn-CreateBox", PlugInConstants.PROCESS_MODEL));
-		addNewModelType(new WmsModel("AKL-Relocate", PlugInConstants.PROCESS_MODEL));
+		addNewModelType(new WmsModel("AKL-TransportiereTablarAway", PlugInConstants.PROCESS_MODEL, "Akl-TransportiereTablarAway-Template"));
+		addNewModelType(new WmsModel("AKL-RequrestTablar", PlugInConstants.PROCESS_MODEL, "Akl-RequrestTablar-Template"));
+		addNewModelType(new WmsModel("AKL-RequrestMaterials", PlugInConstants.PROCESS_MODEL, "Akl-RequrestMaterials-Template"));
+		addNewModelType(new WmsModel("AKL-GetNewContainerForGoods", PlugInConstants.PROCESS_MODEL, "Akl-GetNewContainerForGoods-Template"));
+			
 		addNewModelType(new WmsModel("AKL-AmendAmount", PlugInConstants.PROCESS_MODEL));
 		addNewModelType(new WmsModel("AKL-HandleEmptyBin", PlugInConstants.PROCESS_MODEL));
-		addNewModelType(new WmsModel("AKL-RequestMaterials", PlugInConstants.PROCESS_MODEL, "Akl-RequestMaterials-Template"));
-		addNewModelType(new WmsModel("AKL-RequestTablar", PlugInConstants.PROCESS_MODEL, "Akl-RequestTablar-Template"));
+		addNewModelType(new WmsModel("AKL-C-RequestMaterials", PlugInConstants.PROCESS_MODEL, "Akl-C-RequestMaterials-Template"));
+		addNewModelType(new WmsModel("AKL-C-RequestTablar", PlugInConstants.PROCESS_MODEL, "Akl-C-RequestTablar-Template"));
 		addNewModelType(new WmsModel("AKL-Components", PlugInConstants.COMPONENT_MODEL, "WmsCompositeComponent"));
+		addNewModelType(new WmsModel("AKL-GoodsIn-CreateBox", PlugInConstants.PROCESS_MODEL));
 		this.isProcessVariabile = true;
 		this.isTopologyVariabile = true;
 		
